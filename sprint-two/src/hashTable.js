5fb833a15a4ebe26b590651489f5909d8daec0cd
Kim Dang <kimdang229@gmail.com>
2020-05-15 16:11:59 -0700

    push sprint two

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,12 +1,26 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //check if items in storage is less than 75% of limit
+  //  if not, double storage
+  //  rehash all items
+  //if index isn't in storage
+  //  add to bucket
+  //  increment pointer
+  //else check if the key already exists in bucket
+  //  if so update value in that tuple
+  //else add key value pair to bucket
+  //  increment pointer
+  if (this._size > Math.floor(.75 * this._limit)) {
+    this._storage = this._storage * 2;
+    LimitedArray.each(getIndexBelowMaxForKey(k, this._limit));
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
