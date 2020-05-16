00275a6b6cc9f2047ddf90b0fc30ac25bb349f2f
Kim Dang <kimdang229@gmail.com>
2020-05-16 14:39:29 -0700

    Add bloomFilter

---
+++ sprint-two/src/bloomFilter.js
@@ -0,0 +1,27 @@
+var BloomFilter = function () {
+  var instance = {};
+  var storage = new Array;
+
+  instance.hashFn1 = function () {
+
+  };
+
+  instance.hashFn2 = function () {
+
+  };
+
+  instance.hashFn3 = function () {
+
+  };
+
+  instance.insert = function (value) {
+
+  };
+
+  instance.mightContain = function (value) {
+
+  };
+
+  return instance;
+};
+
