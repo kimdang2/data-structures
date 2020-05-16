21b86b191ee3993f808aefe05926e602f5f1723f
Kim Dang <kimdang229@gmail.com>
2020-05-16 09:38:54 -0700

    Fix hash resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,5 @@
-var HashTable = function() {
-  this._limit = 8;
+var HashTable = function(limit) {
+  this._limit = limit || 8;
   this._storage = LimitedArray(this._limit);
   this._count = 0;
 };
@@ -38,6 +38,8 @@ HashTable.prototype.insert = function(k, v) {
   this._storage.set(index, bucket);
 };
 
+
+
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
@@ -50,14 +52,13 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
-  if (this._count > 8 && this._count - 1 < 0.25 * this._limit ) {
+  if (this._limit > 8 && this._count - 1 < 0.25 * this._limit ) {
     this.resize(-1);
   }
 
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (bucket) {
-    //var tupleIndex = this.findTupleIndex(bucket, k);
     var tupleIndex = bucket.findIndex(function(tuple) {
       return tuple[0] === k;
     });
@@ -70,20 +71,8 @@ HashTable.prototype.remove = function(k) {
 };
 
 HashTable.prototype.resize = function(direction) {
-  // if increasing the size, pass in a 1
-  //    otherwise, pass in a -1
-  // set limit to limit * 2^(value passed)
-  // init newLimitedArray(new limit)
-  // traverse through old limitedArray
-  //   if a bucket exists at index
-  //      then insert into newLimitedArray
-  //  set stroage to newLimitedArray
-
   this._limit = this._limit * Math.pow(2, direction);
-  var resizedStorage = {};
-  resizedStorage._storage = LimitedArray(this._limit);
-  resizedStorage.insert = this.insert;
-  resizedStorage.findTupleIndex = this.findTupleIndex;
+  var resizedStorage = new HashTable(this._limit);
 
   this._storage.each(function(bucket) {
     if (bucket) {
@@ -96,40 +85,6 @@ HashTable.prototype.resize = function(direction) {
   });
   this._storage = resizedStorage._storage;
 };
-
-// HashTable.prototype.resize = function(direction) {
-//   // if increasing the size, pass in a 1
-//   //    otherwise, pass in a -1
-//   // set limit to limit * 2^(value passed)
-//   // init newLimitedArray(new limit)
-//   // traverse through old limitedArray
-//   //   if a bucket exists at index
-//   //      then insert into newLimitedArray
-//   //  set stroage to newLimitedArray
-
-//   this._limit = this._limit * Math.pow(2, direction);
-//   var resizedStorage = {};
-//   // resizedStorage._storage = LimitedArray(this._limit);
-//   // resizedStorage.insert = this.insert;
-//   // resizedStorage.findTupleIndex = this.findTupleIndex;
-
-//   var oldStorage = Object.assign(this._storage);
-//   this._storage = LimitedArray(this._limit);
-//   var context = this;
-//   console.log(this);
-//   oldStorage.each(function(bucket) {
-//     if (bucket) {
-//       _.each(bucket, function(tuple) {
-//         var key = tuple[0];
-//         var value = tuple[1];
-//         context.insert(key, value);
-//       });
-//     }
-//   });
-//   this._storage = resizedStorage._storage;
-// };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
