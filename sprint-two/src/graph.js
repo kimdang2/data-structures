04301f6cbe485b6a82c61e0d1c592aadd7d0e7cb
Kim Dang <kimdang229@gmail.com>
2020-05-17 14:03:14 -0700

    Refactor graph class

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,85 +1,82 @@
+
 // Instantiate a new graph
 var Graph = function() {
-  this.nodes = {};
-  this.edges = {};
+  // A new graph never has any nodes or edges until it's added
+  this._nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
-  this.nodes[node] = {};
-  this.edges[node] = {};
+Graph.prototype.addNode = function(vertex) {
+  // If vertex doesn't exist in Graph, then store the vertex to the graph
+  // If vertex exists, then add edges to that vertex --> graph = { vertex: {edges: {}}  }
+  var graph = this._nodes;
+  graph[vertex] = graph[vertex] || { edges: {} };
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
-  return this.nodes.hasOwnProperty(node);
+Graph.prototype.contains = function(vertex) {
+  var graph = this._nodes;
+  /* if (graph[vertex]) {
+    return true;
+  } else {
+    return false;
+  }*/
+  //return graph[vertex] ? true : false;
+  return !!graph[vertex];
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
-  if (!this.contains(node)) { throw new ReferenceError('Cannot remove a nonexistent node'); }
-
-  var vertexes = this.edges[node];
-  for (var vertex in vertexes) {
-    delete this.edges[vertex][node];
+Graph.prototype.removeNode = function(vertex) {
+  var graph = this._nodes;
+  if (this.contains(vertex)) {
+    // Removes edges between node to be deleted and all other connected nodes.
+    for (var connectingVertex in graph[vertex].edges) { // Traverse each vertex's edges list
+      this.removeEdge(vertex, connectingVertex);
+    }
+    delete graph[vertex];
   }
-  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-  return this.edges[fromNode][toNode] || false;
+Graph.prototype.hasEdge = function(fromVertex, toVertex) {
+  var graph = this._nodes;
+  //return graph[fromVertex].edges[toVertex] || false;
+  return !!graph[fromVertex].edges[toVertex];
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
-  //check that both nodes are in graph
-  //initialize empty edges array
-  //add edge into each node object
-  if (this.contains(fromNode) && this.contains(toNode)) {
-    this.edges[fromNode][toNode] = true;
-    this.edges[toNode][fromNode] = true;
-  }
+Graph.prototype.addEdge = function(fromVertex, toVertex) {
+  var graph = this._nodes;
+  // Add an edge to each vertex pointing to the other vertex
+  graph[fromVertex].edges[toVertex] = true; // {v1 : {edges : {v2: true}}}
+  graph[toVertex].edges[fromVertex] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
-  if (this.contains(fromNode) && this.contains(toNode)) {
-    delete this.edges[fromNode][toNode];
-    delete this.edges[toNode][fromNode];
-  }
+Graph.prototype.removeEdge = function(fromVertex, toVertex) {
+  var graph = this._nodes;
+  // Remove edges from each vertex's edge list
+  delete graph[fromVertex].edges[toVertex];
+  delete graph[toVertex].edges[fromVertex];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for (var node in this.nodes) {
-    cb(node);
+  var graph = this._nodes;
+  for (var vertex in graph) {
+    cb(vertex);
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
-Because we knew the nodes we would be given in the spec were primitive
-we simplified our implementation so that we could perform these processes
-in constant time, however, normally this would not be the case as a node
-is not normally a primitive value.
-
-Since this is a two dimensional data structure we us V to represent the number of Vertexes (nodes) and E to represent the respective number of edges
-
- addNode - O(1)
- contains - O(1)
- removeNode - O(e)
- addEdge - O(1)
- hasEdge - O(1)
- removeEdge - O(1)
- forEachNode - O(v)
-
- If we implemented it to handle node objects we would expect this time complexity
-
-  addNode - O(1)
- contains - O(v)
- removeNode - O(ve)
-
+ *
+ * addNode: O(1)
+ * contains: O(1)
+ * removeNode: O(1)
+ * hasEdge: O(n)
+ * addEdge: O(n)
+ * removeEdge: O(n)
+ * forEachNode: O(n)
+ *
  */
-
-
